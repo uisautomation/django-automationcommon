c26c450d47e44a301c6bba7382591c51f8495993
Abraham Martin <abraham.martin@gmail.com>
2018-02-16 14:06:41 +0000

    Added compatibility with django >2.0 for RequestUserMiddleware

--- automationcommon/middleware.py
+++ automationcommon/middleware.py
@@ -5,6 +5,21 @@ class RequestUserMiddleware(object):
     """
     Middleware that simply set's the request.user to be used for the audit trail.
     """
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+    
     @classmethod
     def process_request(cls, request):
         set_local_user(request.user)
